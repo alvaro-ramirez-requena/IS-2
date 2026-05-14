6ab369dd97d2a66763517f75268ca5ce79c25198
20222396-wq <20222396aloe.ulima.edu.pe>
2026-05-14 18:18:53 -0500

    feat: autenticación completa y validada (registro y login)

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { UserRepository } from "../repositories/user.repository";
 import { UserFactory } from "../factories/user.factory";
+import bcrypt from "bcryptjs";
 
 export class AuthService {
   private userRepository = new UserRepository();
@@ -8,6 +9,7 @@ export class AuthService {
     email: string;
     firstName: string;
     lastName: string;
+    password: string;
   }) {
     // verificar si ya existe
     const existingUser = await this.userRepository.findByEmail(data.email);
@@ -16,20 +18,41 @@ export class AuthService {
       throw new Error("El usuario ya está registrado con ese correo");
     }
 
-    // crear usuario con factory
-    const newUser = UserFactory.create(data);
-
-    // guardar en BD
-    return await this.userRepository.create(newUser);
-  }
-
-  async login(email: string) {
-    const user = await this.userRepository.findByEmail(email);
-
-    if (!user) {
-      throw new Error("Usuario no encontrado");
+  if (data.password.length < 8) {
+      throw new Error("La contraseña es débil. Sugerencia: utiliza al menos 8 caracteres, incluyendo una mayúscula y un número.");
     }
 
-    return user;
+  const hashedPassword = await bcrypt.hash(data.password, 10);
+
+
+    // crear usuario con factory
+   const newUser = UserFactory.create({
+      ...data,
+      password: hashedPassword
+    });
+
+    // guardar en BD
+    await this.userRepository.create(newUser);
+    return { message: "¡Registro completado!" };
+  }
+
+  async login(credentials: { email: string; password: string }) {
+    const user = await this.userRepository.findByEmail(credentials.email);
+
+    if (!user) {
+      throw new Error("Correo o contraseña incorrectos");
+    }
+
+    const isMatch = await bcrypt.compare(credentials.password, user.password);
+    if (!isMatch) {
+      throw new Error("Correo o contraseña incorrectos");
+    }
+    return {
+      message: "Acceso permitido",
+      user: {
+        id: user.id,
+        role: user.role
+      }
+    };
   }
 }
